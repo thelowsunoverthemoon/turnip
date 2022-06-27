5416109985fb177a5685dde0eb4927773ca05c5f
Lowsun <48923877+thelowsunoverthemoon@users.noreply.github.com>
2022-06-27 12:55:16 -0700

    Create header.bat

---
+++ src/header.bat
@@ -0,0 +1,4 @@
+<!-- :: ---------- turnip header v1, thelowsunoverthemoon ---------- ::
+@ECHO OFF&SETLOCAL ENABLEDELAYEDEXPANSION&SET "d[f]=%~1"&SET "d[a]=%*"&CALL :TURNIP_ENGINE&TITLE &GOTO :MAIN
+HTML --><HTML><HEAD><HTA:APPLICATION SCROLL="no" BORDER="thin" CAPTION="no" SHOWINTASKBAR="no" SYSMENU="no" ID="Game">
+<META HTTP-EQUIV="x-ua-compatible" CONTENT="IE=9"><STYLE TYPE="TEXT/Batch">
